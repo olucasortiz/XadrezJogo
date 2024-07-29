adb25a0a567d430b5a8cf7c0b64ad709345cdf24
olucasortiz <lucassas@outlook.com>
2024-07-29 14:49:32 -0300

    1 versao

---
+++ xadrez console/Program.cs
@@ -0,0 +1,15 @@
+﻿using tabuleiro;
+namespace xadrez_console {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(2,2);
+            tab.colocarPeca(?, new Posicao(0,0);
+            tab.colocarPeca(?, new Posicao(1,1));
+            Tela.impirimirTabuleiro(tab);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ xadrez console/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public int qtdMovimento { get; protected set; }
+        public Cor cor { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.qtdMovimento = 0;
+            this.cor = cor;
+            this.tab = tab;
+        }
+    }
+}
